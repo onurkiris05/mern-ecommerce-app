d6d975052c203938df58577456a775b3daf22f5e
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-03-10 10:58:18 +0300

    [Initialized] Backend

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app";
+import env from "./utils/validateEnv";
+import mongoose from "mongoose";
+
+const PORT = env.PORT || 5000;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log("Server is running on port:", PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
