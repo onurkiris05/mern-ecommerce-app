3f3af8aefc1318fb6e4b06aae6542dcfee8e0cc3
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-04-14 21:15:42 +0300

    [Updated] package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,13 @@ import app from "./app";
 import env from "./utils/validateEnv";
 import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 5000;
+const PORT = env.PORT || 5000;
 
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT as number, "0.0.0.0", () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on port: ${PORT}`);
     });
   })
--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -1,7 +1,8 @@
-import { cleanEnv, str } from "envalid";
+import { cleanEnv, port, str } from "envalid";
 
 export default cleanEnv(process.env, {
   MONGO_CONNECTION_STRING: str(),
+  PORT: port(),
   JWT_SECRET: str(),
   STRIPE_SECRET: str(),
 });
